2a053368a726a95ca1629ae43dd2d262404dc3e6
bbt-shivam <bbtshivam@gmail.com>
2026-02-09 09:55:50 +0530

    Cleanup frontend files for a clean api only focus

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('{any}', function () {
-//     return view('welcome');
-// })->where('any', '^(?!api).*$');
-
-Route::fallback(function () {
-    return view('welcome');
-});
